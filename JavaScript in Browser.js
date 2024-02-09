3ca68e74157b6a35a50e12ee6b9c440d9d081b4d
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-02-09 22:36:21 +0530

    JS in Browser

---
+++ JavaScript in Browser.js
@@ -0,0 +1,16 @@
+//JavaScript in the Browser
+//JS was initially created to make web pages alive. JS can be written right in a web pages HTML to make it interactive.
+
+//The browser has an embedded engine called the JavaScript engine or the JavaScript runtime.
+
+//JavaScript's ability in the browser is very limited to protect the users safety. For ex a webpage on http://goofle.com cannot access http://codeswear.com and steal information from there.
+
+//Developer Tools
+//Every browser has some developer tools which make a developeres life easier.
+//F12 on Chrome opens Dev tools.
+/*Elements- Gives all HTML elements
+  Console- Shows all errors + logs
+  Network- All network requests*/
+//We can also write JS commands in the console.
+
+//The Script Tag
